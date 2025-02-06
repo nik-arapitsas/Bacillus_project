d5293a4305c46301edeb487260a3fdd70c4b53d1
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-02-07 01:27:34 +0200

    The graph of the orthogroups shared between any and all isolates is completed

--- scripts/orthofinder_graphs.R
+++ scripts/orthofinder_graphs.R
@@ -100,3 +100,83 @@ ggplot(data_gene_perc, aes(x = Isolates, y = PercentageinOgs)) +
     legend.position = "none"  # Remove the legend
   ) +
   coord_cartesian(expand = FALSE)  # Extend the x-axis
+
+
+# 3 Prepare graph for orthogroups in every isolate with orthogroups in all or any isolates
+
+library(reshape2)
+
+# Read the sorted data into R
+data_gene_all_any <- read.table("/home/nik_arapitsas/Documents/Bacillus_project/Results/orthofinder/Results_Feb03/Graphs/orthogroupcount_in_isolates.txt", header=TRUE, sep = "\t")
+
+# Convert Species to factor to maintain sorting order
+data_gene_all_any$Isolates <- factor(data_gene_perc$Isolates, levels = data$Isolates)
+
+data_long <- melt(data_gene_all_any, id.vars = "Isolates", variable.name = "Category", value.name = "Count")
+
+data_any <- subset(data_long, Category == "Partially.Shared.Orthogroups")   # Blue bars (background)
+data_all <- subset(data_long, Category == "Core.Orthogroups")  # Green bars (foreground)
+
+# Plot without legend
+
+ggplot() +
+  geom_bar(data=data_any, aes(x=Isolates, y=Count), stat="identity", fill="blue") +   # Blue first
+  geom_bar(data=data_all, aes(x=Isolates, y=Count), stat="identity", fill="green") +  # Green on top
+  labs(y="Number of Genes", x="", fill="Category") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        axis.line = element_line(color = "black", size = 0.2),
+        axis.ticks.length = unit(0.1, "cm"),
+        axis.ticks = element_line(color = "black", linewidth = 0.5),
+        legend.position = "right",
+        plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Center align title
+        axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", margin = margin(t = 2.5)),
+        axis.text.y = element_text(hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", 
+          margin = margin(t = 2.0)),
+        plot.margin = margin(10, 10, 10, 10)) +
+  scale_y_continuous(expand = c(0, 0))   # Rotate x-axis labels 
+
+# Plot with legend
+
+data_combined <- rbind(data_any, data_all)
+data_combined$Category <- factor(data_combined$Category, 
+                                 levels = c("Partially.Shared.Orthogroups", "Core.Orthogroups"))
+
+ggplot(data_combined, aes(x = Isolates, y = Count, fill = Category)) +
+  geom_bar(stat = "identity", position = "identity") +  # Maintain layering
+  scale_fill_manual(values = c("Partially.Shared.Orthogroups" = "blue", 
+                               "Core.Orthogroups" = "green"),
+                    labels = c("Partially.Shared.Orthogroups" = "in any species", 
+                               "Core.Orthogroups" = "in all species")) +
+  labs(title = "Number of Genes with Orthogroups", y = "", x = "", fill = "Category") +
+  guides(fill = guide_legend(title = NULL)) +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        axis.line = element_line(color = "black", size = 0.2),
+        axis.ticks.length = unit(0.1, "cm"),
+        axis.ticks = element_line(color = "black", linewidth = 0.5),
+        legend.position = "right",
+        legend.text = element_text(size = 9),  # Reduce font size of legend
+        legend.key.size = unit(0.5, "cm"),  # Adjust size of legend keys (squares)
+        legend.margin = margin(1, 1, 1, 0.1),  # Reduce legend margins
+        plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Center align title
+        axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", 
+                                   margin = margin(t = 2.5)),
+        axis.text.y = element_text(hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", 
+                                   margin = margin(t = 2.0)),
+        plot.margin = margin(10, 1, 1, 0.1)) + 
+  coord_cartesian(expand = FALSE, ylim = c(0,6000))
+  
+
+
+# Draft 3
+
+data_long$Category <- factor(data_long$Category, levels = c("Core.Orthogroups", "Partially.Shared.Orthogroups"))
+# Create the bar plot
+
+ggplot(data_long, aes(x=Isolates, y=Count, fill=Category)) +
+  geom_bar(stat="identity", position="identity", alpha=0.8) +  # Stacked bars with transparency
+  scale_fill_manual(values=c("Partially.Shared.Orthogroups"="blue", "Core.Orthogroups"="green")) +  # Set custom colors
+  labs(y="Number of Genes", x="", fill="Category") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle=45, hjust=1))  # Rotate x-axis labels for better readability
