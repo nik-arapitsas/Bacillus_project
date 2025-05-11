edd81c87094bea1629b87e4ccb0a916e6adb9870
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-05-11 20:01:09 +0300

    Final save of the script to run quast in every isolate

--- scripts/quast_toeveryisolate.sh
+++ scripts/quast_toeveryisolate.sh
@@ -6,18 +6,19 @@
 # Date: 07/05/2025
 # Usage: ./quast_toeveryisolate.sh assemblies_directories.txt 
 
-conda activate quast
-
 for dir in *_assembly/; do
     if [ -d "$dir" ]; then
-        fasta_file="$dir/*_assembly.fasta"
-        output_dir="$dir/${dir:0:6}_quast"
+        basename="${dir%/}"
+        output_dir="${dir}${basename:0:6}_quast"
 
-        if [ -f "$fasta_file" ]; then
+        # Look for a file that ends with 'assembly.fasta' in the directory
+        fasta_file=$(find "$dir" -maxdepth 1 -type f -name '*assembly.fasta' | head -n 1)
+
+        if [ -n "$fasta_file" ]; then
             echo "Running quast on $fasta_file..."
             quast "$fasta_file" -o "$output_dir"
         else
-            echo "Warning: $fasta_file not found, skipping."
+            echo "Warning: No file ending in 'assembly.fasta' found in $dir, skipping."
         fi
     fi
 done
