46c88994349c434f875425f050de74b2bb7b22af
Nikolaos Arapitsas <user@botany2.biology.uoc.gr>
2025-06-11 13:10:12 +0300

    correction of the path

--- scripts/mkdir_for_busco.sh
+++ scripts/mkdir_for_busco.sh
@@ -1,6 +1,6 @@
-project_bacillus = "/media/sarlab/DATA/Bacillus_project/"
+project_bacillus = "/media/sarlab/DATA/Bacillus_project/SRL*/"
 
-for dir in project_bacillus/SRL*/; do
+for dir in $project_bacillus; do
     if [ -d "$dir" ]; then
         shortname=$(basename "$dir")
         mkdir "$shortname"
