1330a2c0dcc8e53f10108ae4612d9f434cd0be2f
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-07-03 01:00:52 +0300

    Prepare the script to run a test

--- scripts/collect_bakta_proteins.sh
+++ scripts/collect_bakta_proteins.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+######################################################################################################
+# script name: collect_bakta_proteins.sh
+# developed by: Nikolaos P. Arapitsas
+# framework: SarrisLab
+######################################################################################################
+# GOAL:
+# Aim of this script is to collect all the 25 proteome files from bakta output in the  
+# /media/sarlab/DATA/Bacillus_project_proteins directory
+######################################################################################################
+# usage:./collect_bakta_proteins.sh
+# complete path: /home/nik_arapitsas/Documents/Bacillus_project/scripts/collect_bakta_proteins.sh
+######################################################################################################
+
 
 base_dir="/media/sarlab/DATA/Bacillus_project"
 protein_dir="${base_dir}/Bacillus_project_proteins"
 mkdir -p "$protein_dir"
 
 # Loop through the isolates (assuming they are in SRL662 or other folders directly inside base_dir)
-for bakta_dir in "${base_dir}"/SRL*/*_bakta; do
+for bakta_dir in "${base_dir}"/SRL368/*_bakta; do
     if [[ -d "$bakta_dir" ]]; then
         shortname=$(basename "$bakta_dir" _bakta)
         protein_file="${bakta_dir}/${shortname}_bakta.faa"
