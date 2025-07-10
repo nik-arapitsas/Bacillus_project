0532f812a0b0d66de5126a271bbf7947abfa7ff3
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-07-10 16:22:33 +0300

    Minor correction to avoid an additional empty txt file

--- scripts/panacota_extract_unique_accessory_genes.sh
+++ scripts/panacota_extract_unique_accessory_genes.sh
@@ -12,39 +12,37 @@ lstfile=$(find ./${X}_pangenome -type f -name "PanGenome*.tsv.lst")
 mapfile=$(find ./${X}_annotation_output -type f -name "LSTINFO-*list_genomes.lst")
 
 # Loop through each genome ID used internally by PanACoTA
-cut -f1 "$mapfile" | while read -r internal_id; do
+tail -n +2 "$mapfile" | cut -f1 | while read -r internal_id; do
 
   # Get the original filename (second column)
   orig_name=$(awk -v id="$internal_id" '$1 == id {print $2}' "$mapfile")
 
   # Determine output-friendly isolate ID
-    if [[ $orig_name == SRL* ]]; then
-      outname=$(echo "$orig_name" | cut -d'_' -f1)
-    elif [[ $orig_name == GCF* ]]; then
-      outname=$(echo "$orig_name" | awk -F'_' '{print $1 "_" $2}')
-    else
-      outname=$orig_name
-    fi
+  if [[ $orig_name == SRL* ]]; then
+    outname=$(echo "$orig_name" | cut -d'_' -f1)
+  elif [[ $orig_name == GCF* ]]; then
+    outname=$(echo "$orig_name" | awk -F'_' '{print $1 "_" $2}')
+  else
+    outname=$orig_name
+  fi
 
-    # Extract unique genes present only in this genome
-    awk -v target="$internal_id" -v OFS="\t" '
-    {
-      keep = 1
-      genes = ""
-      for (i = 2; i <= NF; i++) {
-        if ($i != "-") {
-          if (index($i, target) != 1) {
-            keep = 0
-            break
-          } else {
-            genes = genes ? genes OFS $i : $i
-          }
+  # Extract unique genes present only in this genome
+  awk -v target="$internal_id" -v OFS="\t" '
+  {
+    keep = 1
+    genes = ""
+    for (i = 2; i <= NF; i++) {
+      if ($i != "-") {
+        if (index($i, target) != 1) {
+          keep = 0
+          break
+        } else {
+          genes = genes ? genes OFS $i : $i
         }
       }
-      if (keep && genes != "")
-        print $1, genes
-    }' "$lstfile" > ${X}_accessory/${outname}_unique_genes.txt
+    }
+    if (keep && genes != "")
+      print $1, genes
+  }' "$lstfile" > ${X}_accessory/${outname}_unique_genes.txt
 
-  else
-    echo "⚠️  Warning: No matching orig_name found for $internal_id — skipping."
-  fi
+done
