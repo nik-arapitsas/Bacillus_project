baed1eefb960e9cc54b7dd1e095f46a263b08f41
Nikolaos Arapitsas <user@client-8-119.access.uoc.gr>
2025-07-21 16:51:29 +0300

    Preparation of the script for the pangenome plots

---
+++ scripts/pangenome_graphs.R
@@ -0,0 +1,11 @@
+######################################################################################################
+# script name: pangenome_graphs.R
+# developed by: Nikolaos P. Arapitsas
+# framework: SarrisLab
+######################################################################################################
+# GOAL:
+# Aim of this script is to create the collective barplot and upside plots of the pangenome analysis
+######################################################################################################
+# usage:./pangenome_graphs.R
+# complete path: /home/nik_arapitsas/Documents/Bacillus_project/scripts/pangenome_graphs.R
+######################################################################################################
