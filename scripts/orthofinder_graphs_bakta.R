3c0340b206c2fb1ec6175fa962417a3415bf3e40
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-07-25 19:06:12 +0300

    Change the name of the third part of the figure

--- scripts/orthofinder_graphs_bakta.R
+++ scripts/orthofinder_graphs_bakta.R
@@ -164,9 +164,9 @@ SharedOgs_plot <- ggplot(data_combined, aes(x = Isolates, y = Count, fill = Cate
   geom_bar(stat = "identity", position = "identity") +  # Maintain layering
   scale_fill_manual(values = c("Partially.Shared.Orthogroups" = "#7d92e7", 
                                "Core.Orthogroups" = "#f8d48c"),
-                    labels = c("Partially.Shared.Orthogroups" = "in any isolate", 
-                               "Core.Orthogroups" = "in all isolates")) +
-  labs(title = "Number of Genes with Orthogroups", y = "", x = "", fill = "Category") +
+                    labels = c("Partially.Shared.Orthogroups" = "Shared (≥2 isolates)",
+      "Core.Orthogroups" = "Core (all isolates)")) +
+  labs(title = "Number of Genes Assigned to Orthogroups", y = "", x = "", fill = "Category") +
   guides(fill = guide_legend(title = NULL, byrow = TRUE)) +
   scale_x_discrete(expand=expansion(add=c(.8, .8))) +
   scale_y_continuous(expand=expansion(add=c(0, 0))) +
