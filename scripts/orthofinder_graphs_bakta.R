f2e421314de3eef00168018f50c62a5ee982dd0a
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-07-26 19:10:20 +0300

    Correction in the script to have the correct order in the plot C

--- scripts/orthofinder_graphs_bakta.R
+++ scripts/orthofinder_graphs_bakta.R
@@ -128,31 +128,14 @@ library(reshape2)
 data_gene_all_any <- read.table("/media/sarlab/DATA/Bacillus_project/Bacillus_project_orthofinder/Results_Jul03/Graphs/orthogroupcount_in_isolates.txt", header=TRUE, sep = "\t")
 
 # Convert Species to factor to maintain sorting order
-data_gene_all_any$Isolates <- factor(data_gene_perc$Isolates, levels = data$Isolates)
+
+data_gene_all_any$Isolates <- factor(data_gene_all_any$Isolates, levels = data$Isolates)
+data_gene_all_any <- data_gene_all_any[order(data_gene_all_any$Isolates), ]
 
 data_long <- melt(data_gene_all_any, id.vars = "Isolates", variable.name = "Category", value.name = "Count")
 
 data_any <- subset(data_long, Category == "Partially.Shared.Orthogroups")   # Blue bars (background)
 data_all <- subset(data_long, Category == "Core.Orthogroups")  # Green bars (foreground)
-
-# Plot without legend
-
-ggplot() +
-  geom_bar(data=data_any, aes(x=Isolates, y=Count), stat="identity", fill="blue") +   # Blue first
-  geom_bar(data=data_all, aes(x=Isolates, y=Count), stat="identity", fill="green") +  # Green on top
-  labs(y="Number of Genes", x="", fill="Category") +
-  theme_minimal() +
-  theme(panel.grid = element_blank(),
-        axis.line = element_line(color = "black", size = 0.2),
-        axis.ticks.length = unit(0.1, "cm"),
-        axis.ticks = element_line(color = "black", linewidth = 0.5),
-        legend.position = "right",
-        plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Center align title
-        axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", margin = margin(t = 2.5)),
-        axis.text.y = element_text(hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", 
-          margin = margin(t = 2.0)),
-        plot.margin = margin(10, 10, 10, 10)) +
-  scale_y_continuous(expand = c(0, 0))   # Rotate x-axis labels 
 
 # Plot with legend
 
@@ -161,12 +144,12 @@ data_combined$Category <- factor(data_combined$Category,
                                  levels = c("Partially.Shared.Orthogroups", "Core.Orthogroups"))
 
 SharedOgs_plot <- ggplot(data_combined, aes(x = Isolates, y = Count, fill = Category)) +
-  geom_bar(stat = "identity", position = "identity") +  # Maintain layering
+  geom_bar(stat = "identity", position = "stack") +  # Maintain layering
   scale_fill_manual(values = c("Partially.Shared.Orthogroups" = "#7d92e7", 
                                "Core.Orthogroups" = "#f8d48c"),
-                    labels = c("Partially.Shared.Orthogroups" = "Shared (≥2 isolates)",
-      "Core.Orthogroups" = "Core (all isolates)")) +
-  labs(title = "Number of Genes Assigned to Orthogroups", y = "", x = "", fill = "Category") +
+                    labels = c("Partially.Shared.Orthogroups" = "Partially Shared Orthogroups",
+      "Core.Orthogroups" = "Core Orthogroups")) +
+  labs(title = "Number of Orthogroups per Isolate", y = "", x = "", fill = "Category") +
   guides(fill = guide_legend(title = NULL, byrow = TRUE)) +
   scale_x_discrete(expand=expansion(add=c(.8, .8))) +
   scale_y_continuous(expand=expansion(add=c(0, 0))) +
@@ -180,7 +163,7 @@ SharedOgs_plot <- ggplot(data_combined, aes(x = Isolates, y = Count, fill = Cate
         legend.text = element_text(size = 10),  # Reduce font size of legend
         legend.spacing.y = unit(0.2, 'cm'),
         legend.key.size = unit(0.5, "cm"),  # Adjust size of legend keys (squares)
-        legend.margin = margin(-2, 15, 4, 1.2),  # Reduce legend margins
+        legend.margin = margin(-2, 18, 4, 1.2),  # Reduce legend margins
         plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Center align title
         axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 0.5, size = 10, color = "black", family = "sans", 
                                    margin = margin(t = 2.5)),
@@ -198,15 +181,16 @@ ggsave(paste0("/media/sarlab/DATA/Bacillus_project/Bacillus_project_orthofinder/
        device="png")
   
 collective_orthofinder_graph <- ggarrange(IsolateSpecificOgs_plot, PercOfOgs_plot, SharedOgs_plot,
-          labels = c("A", "B", "C"),                                
+          labels = c("A", "B", "C"),
+          font.label = list(family = "Helvetica", size = 18, face = "plain"),                                
           ncol = 1, 
           nrow = 3, 
           align = "v")  # Align vertically by x-axis
 
 ggsave(paste0("/media/sarlab/DATA/Bacillus_project/Bacillus_project_orthofinder/Results_Jul03/Graphs/collective_orthofinder_graph",".png"),
        plot=collective_orthofinder_graph, 
-       height = 30, 
-       width = 30,
+       height = 35, 
+       width = 35,
        dpi = 300, 
        units="cm",
        device="png")
