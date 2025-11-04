6e89e3d791d3e9756df7f38763a627e2b57d7437
nik-arapitsas <nikolaos_arapitsas@imbb.forth.gr>
2025-11-04 23:00:58 +0200

    Only scripts used in the publication are kept in the repo

--- scripts/runcanu.sh
+++
@@ -1,6 +0,0 @@
-threads=18  # set as appropriate for your system (no more than 128)
-genome_size=4224919
-
-for i in 01 02 03 04; do
-    /home/nik_arapitsas/Documents/Bacillus_project/scripts/canu.sh SRL662_subsampled_reads/sample_"$i".fastq SRL662_assemblies/SRL662_canu_"$i" "$threads" "$genome_size"
-done
--- scripts/unzip_antismash_files.sh
+++
@@ -1,8 +0,0 @@
-for zipfile in *_assembly_Antismash.zip; do
-    num=$(echo "$zipfile" | grep -oE '^[0-9]+')
-    outdir="SRL${num}_assembly_Antismash"
-
-    mkdir -p "$outdir"
-    unzip "$zipfile" -d "$outdir"
-    rm "$zipfile"
-done
